b23edbdd0dd40c45ac5b0e6d2cdfc2b2c8a51ddb
Ryotaro "Justin" Miura <1560508+ryoppippi@users.noreply.github.com>
2019-10-23 13:43:44 +0900

    solving compile error
    while compiling in my environment, it says 
    
    pictorial.c:593:31: fatal error: numpy/arrayobject.h: No such file or directory
    
    These changes solve it.

--- src/m_lib/setup.py
+++ src/m_lib/setup.py
@@ -1,6 +1,7 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import numpy
 
 ext_modules=[ Extension("pictorial",
               ["pictorial.pyx"],
@@ -10,4 +11,5 @@ ext_modules=[ Extension("pictorial",
 setup(
   name = "pictorial",
   cmdclass = {"build_ext": build_ext},
+  include_dirs = [numpy.get_include()]
   ext_modules = ext_modules)
