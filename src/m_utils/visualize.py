41abb5e3a0f975efcd43c3057be68bba13cc660d
Kai Su <67154044+kai3316@users.noreply.github.com>
2021-07-30 21:06:33 +0900

    Update visualize.py
    change for different environments.

--- src/m_utils/visualize.py
+++ src/m_utils/visualize.py
@@ -1,4 +1,8 @@
-
+import os
+import matplotlib as mpl
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using non-interactive Agg backend')
+    mpl.use('Agg')
 import matplotlib.pyplot as plt
 import seaborn as sns
 from PIL import Image
